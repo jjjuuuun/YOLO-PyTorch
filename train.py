0c859460ee71d98d99204cf3edfb86d3201ba39b
jjjuuuun <kjy961226@gmail.com>
2023-04-01 19:11:07 +0900

    mAP에서 막혔다

--- train.py
+++ train.py
@@ -2,6 +2,20 @@ import torch
 import numpy as np
 import random
 import os
+from PIL import Image
+from torch.utils.data  import Dataset, DataLoader, random_split
+import torchvision
+from torchvision import transforms
+from torchvision.transforms.functional import to_pil_image
+from loss import *
+from utils import *
+from datasets import *
+from models import *
+import matplotlib.pyplot as plt
+import wandb
+import torch.optim as optim
+import time
+from pathlib import Path
 
 def torch_seed(random_seed):
     torch.manual_seed(random_seed)
@@ -14,4 +28,147 @@ def torch_seed(random_seed):
     np.random.seed(random_seed)
     random.seed(random_seed)
     os.environ['PYTHONHASHSEED'] = str(random_seed)
+
+# CONSTANT VALUE
+RANDOM_SEED = 223
+BATCH_SIZE = 3
+S = 7
+C = 20
+B = 2
+LEARNING_RATE = 2e-5
+WEIGHT_DECAY = 0
+EPOCHS = 1
+TRAIN_VAL_SPLIT = 0.8
+USE_WANDB = False
+
+
+if USE_WANDB:
+    print("Using wandb ...")
+    # wandb.init(project=opt.wandb_project, entity=opt.wandb_entity)
+    # wandb.config.update(opt)
+
+print("Fixing Seed")
+torch_seed(RANDOM_SEED)
+
+base_dir = Path.cwd()
+data_dir = base_dir/'datasets'
+csv_file = data_dir / '100examples.csv'
+img_dir = data_dir / 'images'
+label_dir = data_dir / 'labels'
+log_dir = base_dir / 'checkpoints'
+
+if not log_dir.exists():
+    print('Make log_dir')
+    log_dir.mkdir()
+
+transform = transforms.Compose([transforms.Resize((448,448)),
+                                transforms.ToTensor()])
+train_set, val_set = random_split(VOCDataset(csv_file, img_dir, label_dir, 
+                                             S=7, B=1, C=20, transform=transform),
+                                  [TRAIN_VAL_SPLIT, 1-TRAIN_VAL_SPLIT])
+print(f'The number of training images = {len(train_set)}')
+print(f'The number of validation images = {len(val_set)}')
+
+train_iter = DataLoader(train_set,
+                    batch_size=BATCH_SIZE,
+                    shuffle = True)
+val_iter = DataLoader(val_set,
+                    batch_size=BATCH_SIZE,
+                    shuffle = True)
+print("Split training dataset and testing dataset")
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(f'The device is ready\t>>\t{device}')
+
+model = mdoel = DarkNet(S, C, B).to(device)
+print('The model is ready ...')
+print(summary(model, (3, 448, 448)))
+
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE,weight_decay=WEIGHT_DECAY)
+# lr_scheduler
+criterion = YOLOLoss(S, B, C)
+
+print("Starting training ...")
+
+for epoch in range(EPOCHS):
+    start_time = time.time()
+    train_epoch_loss = 0
+    train_bboxes = []
+    train_bbox = []
+
+    model.train()
+    for train_img, train_target in train_iter:
+        train_img, train_target = train_img.to(device), train_target.to(device)
+
+        optimizer.zero_grad()
+        train_pred = model(train_img)
+
+        train_iter_loss = criterion(train_pred, train_target)
+        train_iter_loss.backward()
+        optimizer.step()
+        
+        train_epoch_loss += train_iter_loss
+
+        pred_bbox = choice_boxes(train_img, train_pred, S, C, B, count='1ofB', scale=False)
+        target_bbox = choice_boxes(train_img, train_target, S, C, B=1, count='1of1', scale=False)
+        
+        pred_bbox = cell_to_boxes(pred_bbox, S)
+        # dects = [d for d in detections if d[1] == c]
+        for d in pred_bbox[0]:
+            print(d[1])
+        # print(len(pred_bbox), len(pred_bbox[0]))
+        # target_bbox = cell_to_boxes(target_bbox, S)
+        # print(len(target_bbox), len(target_bbox[0]))
+
+        break
+    break
+    #     # ======================================== #
+    #     # Bounding Box가 어떻게 학습되고 있는지 확인 #
+    #     pred_bbox = choice_boxes(train_img, train_pred, S, C, B, count='BofB', scale=True)
+    #     pil_image = draw_image(train_img[0], pred_bbox[0], S, B, count='BofB')
+    #     image = wandb.Image(pil_image, caption=f"Training Bounding Boxes {epoch}")
+    #     train_bboxes.append(image)
+
+    #     pred_bbox = choice_boxes(train_img, train_pred, S, C, B, count='1ofB', scale=True)
+    #     pil_image = draw_image(train_img[0], pred_bbox[0], S, B=1, count='BofB')
+    #     image = wandb.Image(pil_image, caption=f"Choice Bounding Box {epoch}")
+    #     train_bbox.append(image)
+    #     # ======================================== #
+
+    # train_epoch_loss = train_epoch_loss / len(train_iter)
+    # pred_bbox = choice_boxes(train_img, train_pred, S, C, B, count='1of1', scale=False)
+    
+    # # Validation
+    # with torch.no_grad():
+    #     val_epoch_loss = 0
+    #     model.eval()
+    #     for val_img, val_target in val_iter:
+    #         val_img, val_target = val_img.to(device), val_target.to(device)
+
+    #         val_pred = model(val_img)
+    #         val_iter_loss = criterion(val_pred, val_target).detach()
+
+    #         val_epoch_loss += val_iter_loss
+    #     model.train()
+
+    # print('time >> {:.4f}\tepoch >> {:04d}\ttrain_loss >> {:.4f}\\tval_loss >> {:.4f}'
+    #       .format(time.time()-start_time, epoch, train_epoch_loss, val_epoch_loss))
+    
+    # if (epoch+1) % 5 == 0:
+    #     torch.save({
+    #         'epoch': epoch,
+    #         'model_state_dict': model.state_dict(),
+    #         'optimizer_state_dict': optimizer.state_dict(),
+    #         }, log_dir / f'epoch({epoch})_loss({train_epoch_loss:.3f}).pt')
+    # if USE_WANDB:
+    #     wandb.log({'train_loss': train_epoch_loss,
+    #                'val_loss': val_epoch_loss,
+    #                'train_bboxes': train_bboxes,
+    #                'train_bbox': train_bbox})
+
+    # conv_targets = convert_scale(target, imgs, S)
+
+    # for idx, (img, conv_target) in enumerate(zip(imgs, conv_targets)):
+    #     img = plot_image(img, conv_target, S)
+    #     img.save(f'./test_images/output_{idx}.png', 'png')
 
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from torchvision.transforms.functional import to_pil_image
 from PIL import Image, ImageDraw, ImageFont
+from collections import Counter
 
 CLASSES = [
     "aeroplane",
@@ -57,59 +58,139 @@ def IoU(bbox1, bbox2):
 
     return intersection / (bbox1_area + bbox2_area - intersection + 1e-6)
 
-def convert_cell(pred, S, C, B):
-    batch_size= pred.size(0)
-    pred = pred.reshape(batch_size, S, S, C+5*B)
-    return pred
+def mAP(detections, ground_truths, C, iou_threshold=0.5):
+    ap = [] # Average Precision
+    epsilon = 1e-6
 
-def convert_scale(output, img, S):
+    for c in range(C):
+        dects = [d for d in detections if d[1] == c]
+        gts = [g for g in detections if g[1] == c]
+
+        # Recall의 분모
+        len_gts = len(gts)
+
+        # Confidence score에 따라 내림차순 정렬
+        dects = sorted(dects, key=lambda conf : conf[2], reverse=True)
+
+        TP = np.zeros(len(dects))
+        FP = np.zeros(len(dects))
+        
+        # 각 image에서 class가 c인 ground truth box 수
+        n_box = Counter(x[0] for x in gts)
+        for key, val in n_box.items():
+            n_box = torch.zeros(val)
+
+        # 
+        for i in range(len(dects)):
+            gt = [gt for gt in gts if gt[0] == dects[i][0]]
+
+
+def cell_to_boxes(output, S):
+    convert_output = output.reshape(output.shape[0], S*S, -1)
+    convert_output[..., 0] = convert_output[..., 0].long()
+    all_bboxes = []
+
+    for batch in range(output.shape[0]):
+        bboxes = []
+
+        for idx in range(S*S):
+            bboxes.append([x.item() for x in convert_output[batch, idx, :]])
+        all_bboxes.append(bboxes)
+    return all_bboxes
+
+def choice_boxes(img, output, S, C, B, count='1ofB', scale=False):
+    batch_size = output.shape[0]
+    output = output.reshape(batch_size, S, S, C+5*B)
     label = output[..., :20].argmax(-1).unsqueeze(-1)
-    score = output[..., 20:21]
-    bbox = output[..., 21:25]
-    batch_size, _, width, height = img.shape
+    if count == 'BofB':
+        if scale:
+            output[...,21:25] = convert_scale(output[...,21:25], img)
+            output[...,26:30] = convert_scale(output[...,26:30], img)
+        return torch.cat((label, output[...,20:]), dim=-1)
+    else:
+        if count == '1ofB':
+            scores = torch.cat(
+                (output[..., 20].unsqueeze(0), output[..., 25].unsqueeze(0)), dim=0
+            )
 
-    cell_indices = torch.arange(7).repeat(batch_size, 7, 1).unsqueeze(-1)
+            bbox1 = output[..., 21:25]
+            bbox2 = output[..., 26:30]
+            score, idx = torch.max(scores, dim=0)
+            score, idx = score.unsqueeze(-1), idx.unsqueeze(-1)
+            bbox = (1 - idx) * bbox1 + idx * bbox2
+        elif count == '1of1':
+            score = output[..., 20:21]
+            bbox = output[..., 21:25]
+
+        if scale:
+            bbox = convert_scale(bbox, img)
+
+        return torch.cat((label, score, bbox), dim = -1)
+
+
+def convert_scale(bbox, img):
+    """bounding box의 scale을 이미지에 맞게 조정
+
+    Args:
+        output (_type_): bounding box : [batch, S, S, 4]
+        img (_type_): [batch, 3, 448, 448]
+        S (_type_): 7
+    """
+    batch_size, S, _, vec_length = bbox.shape
+    _, _, width, height = img.shape
+
+    cell_indices = torch.arange(S).repeat(batch_size, S, 1).unsqueeze(-1)
     x = 1 / S * (bbox[..., :1] + cell_indices)
     y = 1 / S * (bbox[..., 1:2] + cell_indices.permute(0, 2, 1, 3))
     w = 1 / S * bbox[..., 2:3]
     h = 1 / S * bbox[..., 3:4]
-    print(score.shape, x.shape, w.shape)
-    convert_bbox = torch.cat((label, score, x,y,w,h), dim = -1)
 
-    convert_bbox[...,2] = (convert_bbox[...,2] - convert_bbox[...,4] / 2) * width
-    convert_bbox[...,3] = (convert_bbox[...,3] - convert_bbox[...,5] / 2) * height
-    convert_bbox[...,4] = convert_bbox[...,4] * width
-    convert_bbox[...,5] = convert_bbox[...,5] * height
+    bboxes = torch.cat((x,y,w,h), dim = -1)
 
-    return convert_bbox
+    bboxes[...,0] = (bboxes[...,0] - bboxes[...,2] / 2) * width
+    bboxes[...,1] = (bboxes[...,1] - bboxes[...,3] / 2) * height
+    bboxes[...,2] = bboxes[...,2] * width
+    bboxes[...,3] = bboxes[...,3] * height
 
+    return bboxes
 
-def plot_image(image, target, S):
-    """Plots predicted bounding boxes on the image"""
-    img = to_pil_image(image)
+def plot_images(images, targets, S, B, count):
+    for image, target in zip(images, targets):
+        img = draw_image(image, target, S, B, count)
+        plt.imshow(np.array(img))
+        plt.show()
+
+def draw_image(img, target, S, B, count):
+    img = to_pil_image(img)
     draw = ImageDraw.Draw(img)
     colors = np.random.randint(0, 255, size=(80,3), dtype=np.uint8)
     font = ImageFont.truetype("C:\Windows\Fonts\Arial.ttf")
-    
-    # Create a Rectangle potch
-    for i in range(S):
-        for j in range(S):
-            label, score, x,y,w,h = target[i][j]
-            if score != 0:
-                color = colors[int(label)]
-                name = CLASSES[int(label)]
-                draw.rectangle(((x, y), 
-                                (x+w, y+h)), 
-                                outline=tuple(color), 
-                                width = 3)
-                
 
-                tw, th = font.getsize(name)
-                draw.rectangle(((x, y), 
-                                (x+tw, y+th)),
-                                fill = tuple(color + [0]))
-                draw.text((x, y), name, fill=(255,255,255,0), font=font, align='center')
+    if count == 'BofB':
+        for i in range(S):
+            for j in range(S):
+                label, *bbox = target[i][j]
+                for m in range(B):
+                    score, x, y, w, h = bbox[m*5:(m+1)*5]
+                    draw.rectangle(((x, y), 
+                                    (x+w, y+h)), 
+                                    outline=(0,0,0), 
+                                    width = 3)
+    else:
+        for i in range(S):
+            for j in range(S):
+                label, score, x,y,w,h = target[i][j]
+                if score != 0:
+                    color = colors[int(label)]
+                    name = CLASSES[int(label)]
+                    draw.rectangle(((x, y), 
+                                    (x+w, y+h)), 
+                                    outline=tuple(color), 
+                                    width = 3)
+                    tw, th = font.getsize(name)
+                    draw.rectangle(((x, y), 
+                                    (x+tw, y+th)),
+                                    fill = tuple(color + [0]))
+                    draw.text((x, y), name, fill=(255,255,255,0), font=font, align='center')
 
-    # plt.imshow(np.array(img))
-    # plt.show()
     return img    
